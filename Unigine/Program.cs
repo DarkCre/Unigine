9ea37beebee37343e34e9170a8efb8ce6021d656
DarkCre <113008949+DarkCre@users.noreply.github.com>
2022-10-17 15:11:36 +0300

    Add files via upload

---
+++ Unigine/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Unigine
+{
+    internal class Program
+    {
+        /*static bool ReadingFile()
+        {
+            try
+            {
+                FileStream fs = new FileStream("ReadAndRewrite.txt", FileMode.Open);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        */
+        static void ReadingFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("ReadAndRewrite.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при чтении файла!");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void WritingStringFile()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter ("ReadAndRewrite.txt",true))
+                {
+                    sw.WriteLine(Console.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Критическая ошибка!");
+                Console.WriteLine(e.Message);
+            }
+        }
+static void Main(string[] args)
+        {
+            ReadingFile();
+            WritingStringFile();
+            WritingStringFile();
+
+        }
+    }
+}
